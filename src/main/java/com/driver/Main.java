c36438ac1534f65e1e10231340445d9b22be7567
Durga Rao Arangi <durga1710504@gmail.com>
2023-05-17 10:40:14 +0530

    pizza solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,7 +10,7 @@ public class Main {
     System.out.println(dp.getBill());
 
     
-    Pizza p = new Pizza(true);
+    Pizza p = new Pizza(false);
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
